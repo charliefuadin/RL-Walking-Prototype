7334e1e405aaf65abfb9d1345ed0cc8797466148
charliefuadin <charliefuadin@gmail.com>
2025-12-02 20:53:08 -0500

    Finished Basic stuff

---
+++ Prototype Walking/Assets/Scripts/WalkBehaviour.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using Unity.MLAgents;
+using Unity.MLAgents.Actuators;
+using Unity.MLAgents.Sensors;
+public class WalkBehaviour : Agent
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
